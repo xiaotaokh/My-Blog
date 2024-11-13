3c5ba0ea94bfad7e61b494a924fe930a4ca31f33
taohang <xitaokh@163.com>
2024-11-13 10:29:25 +0800

    chore: 修改配置文件，基础项目配置信息，页面路径等

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,29 +1,55 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig, loadEnv } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
-export default defineConfig({
-  title: "我的博客",
-  description: "小小的人儿啊，风生水起啊，天天就爱穷开心那",
-  base: "/blog/",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: '首页', link: '/' },
-      { text: '示例', link: '/Examples/markdown-examples' }
-    ],
 
-    sidebar: [
-      {
-        text: '示例',
-        items: [
-          { text: 'Markdown 示例', link: '/Examples/markdown-examples' },
-          { text: 'API 示例', link: '/Examples/api-examples' }
-        ]
-      }
+export default defineConfig(({ command, mode }) => {
+  return {
+    title: "xiaotaokh", // 站点标题(浏览器tab名称：title)
+    titleTemplate: "blog", // 站点标题描述(浏览器tab名称：title | titleTemplate)
+    description: "小小的人儿啊，风生水起啊，天天就爱穷开心那", // 站点的描述 体现在HTML 中的 <meta> 标签。
+    // lang: 'en-US', // 语言 <html lang="en-US">
+    /**
+     * 开发模式下，使用绝对路径
+     * 生产环境下，站点将部署到的 base URL，以 / 开头和结尾，对应https://xiaotaokh.github.io/blog中的blog
+     */
+    base: mode === 'production' ? "/blog/" : "/",
+    // Vite 配置选项
+    vite: {},
+    // @vitejs/plugin-vue 选项
+    vue: {},
+    // markdown解析器配置
+    markdown: {},
+    head: [
+      ['link', { rel: 'stylesheet', href: '//at.alicdn.com/t/c/font_4743040_m7ehua33vvr.css' }], // iconfont
+      ['link', { rel: 'icon', href: '/favicon/favicon.png' }],  // 网站图标，相对于项目根目录，受srcDir字段影响
     ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/xiaotaokh' }
-    ]
+    srcDir: "./src", // markdown文件所在的根目录，相对于项目根目录进行设置。
+    srcExclude: ['**/README.md', '**/TODO.md'], // 用于匹配应排除作为源内容输出的 markdown 文件
+    outDir: "./docs", // 项目的构建输出位置, 或者在package.json 中 scripts.build 配置 vitepress build --outDir docs
+    assetsDir: "static", // 指定放置生成的静态资源的目录。该路径应位于 outDir 内，并相对于它进行解析。
+    cacheDir: "./.vitepress/cache", // 缓存文件的目录，相对于项目根目录
+    metaChunk: true, // 当设置为 true 时，将页面元数据提取到单独的 JavaScript 块中，而不是内联在初始 HTML 中。
+    // 主题配置
+    themeConfig: {
+      // https://vitepress.dev/reference/default-theme-config
+      nav: [
+        { text: '首页', link: '/views/home' },
+        { text: '示例', link: '/views/examples/markdown-examples' }
+      ],
+      
+      sidebar: [
+        {
+          text: '示例',
+          items: [
+            { text: 'Markdown 示例', link: '/views/examples/markdown-examples' },
+            { text: 'API 示例', link: '/views/examples/api-examples' }
+          ]
+        }
+      ],
+      
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/xiaotaokh' }
+      ]
+    }
   }
 })
---
+++ src/views/home/index.md
@@ -0,0 +1,53 @@
+---
+# https://xiaotaokh.github.io/blog
+
+# 页面的布局, home 主页的特殊布局  doc 默认文档样式 page 自定义布局
+layout: home
+
+# 当 layout 设置为 home 时，定义主页顶部的内容
+hero:
+  name: "xiaotaokh"
+  text: "vitePress"
+  tagline: 我的伟大项目标语
+  image:
+    src: /assets/home/home.jpeg
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /views/examples/markdown-examples
+    - theme: alt
+      text: API 示例
+      link: /views/examples/api-examples
+
+# 当 layout 设置为 home 时，定义主页底部特色内容
+features:
+  - icon: icon-gongneng-copy  # 图标
+    title: 特征 A  # 标题
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit  # 详情
+    link: /views/examples/markdown-examples/a  # 外部内部链接
+    linkText: 更多 # 链接提示
+    
+  - title: 功能 B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 功能 C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
+
+
+<script setup>
+import { useData } from 'vitepress'
+
+const { theme, page, frontmatter } = useData()
+</script>
+
+[//]: # (<pre>{{ frontmatter }}</pre>)
+<i class="iconfont icon-gongneng-copy"></i>
+
+<style lang="scss">
+  :root {
+    --vp-home-hero-name-color: transparent;
+    --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe, #41d1ff);
+  }
+</style>
